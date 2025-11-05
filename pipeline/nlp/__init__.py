6ad0a55785f7d327833db161c3b9209c6c3b5015
jhgit0219 <81949308+jhgit0219@users.noreply.github.com>
2025-11-05 22:55:54 +0800

    chore: update nlp package exports

--- pipeline/nlp/__init__.py
+++ pipeline/nlp/__init__.py
@@ -1,5 +1,7 @@
 """NLP utilities for entity extraction and text analysis."""
 
 from .entity_extractor import EntityExtractor
+from .character_inference import CharacterInferenceEngine
+from .keyword_extractor import KeywordExtractor
 
-__all__ = ["EntityExtractor"]
+__all__ = ["EntityExtractor", "CharacterInferenceEngine", "KeywordExtractor"]
